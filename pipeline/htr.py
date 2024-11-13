65e00daba012acb6d82b4dc05aeb5054730cfb83
Henry Heino <personalizedrefrigerator@gmail.com>
2024-11-12 20:16:22 -0800

    WIP: Full HTR pipeline (English)

---
+++ pipeline/htr.py
@@ -0,0 +1,46 @@
+from PIL import Image, ImageDraw
+import PIL
+import PIL.ImageDraw
+from kraken import blla
+
+# NOTE: Requires Python 3.11.
+
+
+def mask_image_with_polygon(image: PIL.Image, polygon: list[tuple]):
+	mask = Image.new('1', image.size, 0) # Black/white image
+	draw = ImageDraw.Draw(mask)
+	draw.polygon(polygon, fill = 1)
+	background = Image.new('RGB', image.size, color=(255, 255, 255))
+	# Adds a clipped version of the original image to the background:
+	background.paste(image, mask=mask)
+	return background
+
+def crop_image_to_polygon(image: PIL.Image, polygon: list[tuple]):
+	polygon_bbox_top_x = min(point[0] for point in polygon)
+	polygon_bbox_top_y = min(point[1] for point in polygon)
+	polygon_bbox_bottom_x = max(point[0] for point in polygon)
+	polygon_bbox_bottom_y = max(point[1] for point in polygon)
+
+	clipped = mask_image_with_polygon(image, polygon)
+	return clipped.crop(
+		(polygon_bbox_top_x, polygon_bbox_top_y, polygon_bbox_bottom_x, polygon_bbox_bottom_y)
+	)
+
+def segment_image(image: PIL.Image):
+	def process_boundary(boundary):
+		return [ tuple(point) for point in boundary ]
+	boundaries = [ process_boundary(line.boundary) for line in blla.segment(image).lines ]
+	return [
+		crop_image_to_polygon(image, boundary) for boundary in boundaries
+	]
+
+class TextRecogniser:
+	def __init__():
+		# 1. Download the model(s)
+		# 2. Load the models
+		pass
+
+def recognise_text(image: PIL.Image):
+	"""
+	Attepts to recognise handwritten text from the provided [image].
+	"""
